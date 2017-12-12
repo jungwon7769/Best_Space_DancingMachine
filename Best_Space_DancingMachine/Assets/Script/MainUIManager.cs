64922814a23e31798e5346cfcd3ff63a5f8c610f
jungwon7769 <jungwon7769@naver.com>
2017-12-12 14:49:17 +0900

    Main + Place 추가

--- Best_Space_DancingMachine/Assets/Script/MainUIManager.cs
+++ Best_Space_DancingMachine/Assets/Script/MainUIManager.cs
@@ -7,55 +7,66 @@ public class MainUIManager : MonoBehaviour {
 
     public Text money;
     public Text day;
-    public Text place;
+    public Text placeName;
 
     public Button dance;
     public Button sound;
     public Button coordi;
+    public Button place;
 
     public GameObject dancePanel;
     public GameObject soundPanel;
     public GameObject coordiPanel;
+    public GameObject placePanel;
 
 	// Use this for initialization
 	void Start () {
         dancePanel.SetActive(true); // dance default
         soundPanel.SetActive(false);
         coordiPanel.SetActive(false);
+        placePanel.SetActive(false);
 	}
 
     void Update()
     {
         money.text = PlayerData.getInstance().money.ToString();
         day.text = "DAY. " + PlayerData.getInstance().day;
+        placeName.text = PlayerData.getInstance().place;
     }
 
     public void onClick_Dance()
     {
-        panelActive(dancePanel, soundPanel, coordiPanel);
+        panelActive(dancePanel, soundPanel, coordiPanel, placePanel);
 
     }
 
     public void onClick_Sound()
     {
-        panelActive(soundPanel, dancePanel, coordiPanel);
+        panelActive(soundPanel, dancePanel, coordiPanel, placePanel);
     }
 
     public void onClick_Coordi()
     {
-        panelActive(coordiPanel, dancePanel, soundPanel);
+        panelActive(coordiPanel, dancePanel, soundPanel, placePanel);
+    }
+
+    public void onClick_Place()
+    {
+        panelActive(placePanel, dancePanel, soundPanel, coordiPanel);
     }
 
     /// <summary>
-    /// 패널 활성화
+    /// 패널비활, active 빼고 모두비활
     /// </summary>
-    /// <param name="active">활성화</param>
-    /// <param name="panel1">비활</param>
-    /// <param name="panel2">비활</param>
-    public void panelActive(GameObject active, GameObject panel1, GameObject panel2)
+    /// <param name="active"></param>
+    /// <param name="panel1"></param>
+    /// <param name="panel2"></param>
+    /// <param name="panel3"></param>
+    public void panelActive(GameObject active, GameObject panel1, GameObject panel2, GameObject panel3)
     {
         active.SetActive(true);
         panel1.SetActive(false);
         panel2.SetActive(false);
+        panel3.SetActive(false);
     }
 }
--- Best_Space_DancingMachine/Assets/Script/PlayerData.cs
+++ Best_Space_DancingMachine/Assets/Script/PlayerData.cs
@@ -7,11 +7,13 @@ public class PlayerData{
 
     public float money;
     public int day;
+    public string place;
 
     private PlayerData()
     {
         money = 0.0f;
         day = 1;
+        place = "동네놀이터"; 
         //초기화
     }
 
