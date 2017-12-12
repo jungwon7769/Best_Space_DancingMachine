7b9234bf22cc97f4e7e8c5e3c43c0634cba9656f
jungwon7769 <jungwon7769@naver.com>
2017-12-12 14:40:32 +0900

    Main 수정 / 일부 스크립트 추가

---
+++ Best_Space_DancingMachine/Assets/Script/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData{
+    private static PlayerData instance = null;
+
+    public float money;
+    public int day;
+
+    private PlayerData()
+    {
+        money = 0.0f;
+        day = 1;
+        //초기화
+    }
+
+    public static PlayerData getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new PlayerData();
+        }
+        return instance;
+    }
+}
