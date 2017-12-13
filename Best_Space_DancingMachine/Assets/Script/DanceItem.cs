5c6233fabb6b3926e1d819f8f3ee5b32ff9b1b96
jungwon7769 <jungwon7769@naver.com>
2017-12-13 16:03:06 +0900

    돌멩이 추가

--- Best_Space_DancingMachine/Assets/Script/DanceItem.cs
+++ Best_Space_DancingMachine/Assets/Script/DanceItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Item : MonoBehaviour {
+public class DanceItem : MonoBehaviour {
 
     private static Item instance = null;
 
--- Best_Space_DancingMachine/Assets/Script/Sound.cs
+++ Best_Space_DancingMachine/Assets/Script/Sound.cs
@@ -8,7 +8,11 @@ public class Sound : MonoBehaviour {
     public class SoundInfo
     {
         public int num; // 사운드 번호
-        public AudioClip audio;
+        public string name;// 사운드 이름
+        public float price; // 사운드 가격
+        public AudioClip audio; // 사운드
     }
+
+
 
 }
