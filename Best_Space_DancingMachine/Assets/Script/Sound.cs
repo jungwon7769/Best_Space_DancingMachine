7ef5eee67654bf2f4c0054644d0b344d4c9283c6
jungwon7769 <jungwon7769@naver.com>
2017-12-13 15:41:46 +0900

    사운드 수정

--- Best_Space_DancingMachine/Assets/Script/Sound.cs
+++ Best_Space_DancingMachine/Assets/Script/Sound.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sound : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class SoundInfo
+    {
+        public int num; // 사운드 번호
+        public AudioClip audio;
+    }
+
 }
